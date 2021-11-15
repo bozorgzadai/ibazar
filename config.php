a091b83073fe6484fdc176180fa08b3300b53f77
Ali Bozorgzad <A.bozorgzad1995@gmail.com>
2021-11-15 22:53:26 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?
+global $config;
+$config['db']['host'] = 'localhost';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['name'] = 'ibazar';
+
+$config['lang'] = 'fa';
+
+$config['salt1'] = 'zxnmWER@#%^98345@#xvc';
+$config['salt2'] = 'WERa234XCV#$%XCVasdf9';
+$config['base'] = '/ibazar';
+$config['route'] = array(
+  '/home' => '/page/home',
+  '/login' => '/account/login',
+  '/خانه' => '/page/home',
+  '/ورود' => '/account/login',
+);
+
+$config['zarinpal']['merchantId'] = "test";
+$config['FIREBASE_API_KEY'] = "AAAA-JKyxE4:APA91bEvx9C1geseTrfZjR2KjqMlLxczND_nP83y7nSyDBHi36K51j0x2lu7rADiXM5QHArBEivBZR_mbmoapGd0UYJDETSwgIR_c4h2NXxIpA2O1sF9lxY8VqykC0EA7zHYaUkmftLwGae_7QMLFlnAYzMRKEiT-w";
